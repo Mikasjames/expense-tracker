b6d2da3b5d70fb85a7485998db0a8413497710c0
Mikas James <mikasjames@gmail.com>
2024-07-17 10:05:40 +0800

    Modify sidebar bg color, add tooltip

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,11 +3,12 @@ import { AuthService } from '../../services/auth/auth.service';
 import { SidebarContent } from '../../models/util.interface';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbTooltipModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.sass',
 })
