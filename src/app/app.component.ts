7fcf4da474b6a43a46f6ac6b850c5f843fbf365c
Mikas James <mikasjames@gmail.com>
2024-07-17 17:14:35 +0800

    Add ngx echarts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth/auth.service';
+import { provideEcharts } from 'ngx-echarts';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { AuthService } from './services/auth/auth.service';
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
+  providers: [provideEcharts()],
 })
 export class AppComponent implements OnInit {
   constructor(private auth: AuthService) {}
