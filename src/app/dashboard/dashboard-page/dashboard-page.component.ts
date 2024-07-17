ad51a3bd2e977bcd6c5777d9fb9ccf8fbeedf827
Mikas James <mikasjames@gmail.com>
2024-07-17 17:16:06 +0800

    Revert dashboard page

--- src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { UserInterface } from '../../models/user.interface';
+import { TransactionService } from '../../services/transactions/transaction.service';
+import { Transaction } from '../../models/transaction.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.sass',
 })
