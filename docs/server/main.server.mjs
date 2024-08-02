98fdee6af6c96e15c22159451d41884b446c2eb0
Mikas James <mikasjames@gmail.com>
2024-08-02 21:32:47 +0800

    Init deployment

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as a}from"./chunk-4TNCNCAK.mjs";import"./chunk-BFWKVSOP.mjs";import"./chunk-C57S22MH.mjs";import"./chunk-MQIWQKQH.mjs";import"./chunk-H6KHSOBK.mjs";export{a as default};
