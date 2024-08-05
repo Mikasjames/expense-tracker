bacc5190aa277d1cb7decd4489da591adcfb3432
Mikas James <mikasjames@gmail.com>
2024-08-05 16:21:55 +0800

    Add topbar component

---
+++ src/app/components/topbar/topbar.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ToolBarContent } from '../../models/util.interface';
+import { Router } from '@angular/router';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [CommonModule, NgbTooltip],
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.sass',
+})
+export class TopbarComponent {
+  showSidebar = false;
+  @Input() topbarItems: ToolBarContent[] = [
+    {
+      label: 'Show Options',
+      icon: 'bi bi-three-dots-vertical',
+      action: () => {
+        this.showSidebar = !this.showSidebar;
+        this.toggleSidebar.emit(this.showSidebar);
+      },
+    },
+    {
+      label: '',
+      icon: 'flex-grow-spacer',
+      action: () => {},
+    },
+  ];
+  @Output() toggleSidebar = new EventEmitter<boolean>();
+
+  constructor(private router: Router) {}
+}
