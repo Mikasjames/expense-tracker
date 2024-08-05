682fca56145dbf6a77477a83dbeabb3b285e6b18
Mikas James <mikasjames@gmail.com>
2024-08-05 15:39:38 +0800

    Add styles to transaction table to easily determine if transaction or expense

--- src/app/components/transaction-table/transaction-table.component.ts
+++ src/app/components/transaction-table/transaction-table.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import { ColDef, RowClassRules, GridOptions } from 'ag-grid-community';
 import { Transaction } from '../../models/transaction.interface';
-import {UtilService} from "../../services/util/util.service";
-import {TagService} from "../../services/tags/tag.service";
+import { UtilService } from '../../services/util/util.service';
+import { TagService } from '../../services/tags/tag.service';
 
 @Component({
   selector: 'app-transaction-table',
@@ -26,13 +26,16 @@ export class TransactionTableComponent {
     getRowStyle: (params) => {
       const isIncome = params.data.type === 'income';
       const oddRow = (params.node.rowIndex ?? 0) % 2 === 0;
-      return isIncome ? { backgroundColor: oddRow ? '#e6f3e6' : '#f0f8f0' } : { backgroundColor: oddRow ? '#fce4e4' : '#fdf0f0' };
-    }
-  }
+      return isIncome
+        ? { backgroundColor: oddRow ? '#e6f3e6' : '#f0f8f0' }
+        : { backgroundColor: oddRow ? '#fce4e4' : '#fdf0f0' };
+    },
+  };
 
-
-  constructor(private utilService: UtilService, private tagService: TagService) {
-  }
+  constructor(
+    private utilService: UtilService,
+    private tagService: TagService,
+  ) {}
 
   ngOnChanges() {
     this.rowData = this.transactionToRowData(this.transactions);
