e657d1403dec11ee47abb49218b2df2fde81944f
Mikas James <mikasjames@gmail.com>
2024-08-07 10:56:00 +0800

    Add date selector to topbar component

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,13 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  Renderer2,
+} from '@angular/core';
 import { ToolBarContent } from '../../models/util.interface';
 import { Router } from '@angular/router';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
+import { DateSelectorComponent } from '../date-selector/date-selector.component';
+import { DateSelectorService } from '../../services/date-selector/date-selector.service';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [CommonModule, NgbTooltip],
+  imports: [CommonModule, NgbTooltip, DateSelectorComponent],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.sass',
 })
@@ -27,8 +35,41 @@ export class TopbarComponent {
       icon: 'flex-grow-spacer',
       action: () => {},
     },
+    {
+      label: '',
+      icon: 'bi bi-calendar-fill',
+      action: () => {
+        this.toggleDateSelector();
+        this.preventCloseOnClick();
+      },
+    },
   ];
   @Output() toggleSidebar = new EventEmitter<boolean>();
+  showDateSelector = false;
+  dateSelectorClicked = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private dateSelectorService: DateSelectorService,
+    private renderer: Renderer2,
+  ) {
+    this.dateSelectorService.dateRange$.subscribe(() => {
+      this.showDateSelector = false;
+    });
+
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (!this.dateSelectorClicked) {
+        this.showDateSelector = false;
+      }
+      this.dateSelectorClicked = false;
+    });
+  }
+
+  toggleDateSelector() {
+    this.showDateSelector = !this.showDateSelector;
+  }
+
+  preventCloseOnClick() {
+    this.dateSelectorClicked = true;
+  }
 }
