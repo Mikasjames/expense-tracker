11c41e53c9ac11fe242147c6fcab6b1a0f7a7995
Mikas James <mikasjames@gmail.com>
2024-08-07 19:10:37 +0800

    Add form filler component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { AuthGuard, PublicOnlyGuard } from './services/auth.guard';
+import {
+  AdminOnlyGuard,
+  AuthGuard,
+  PublicOnlyGuard,
+} from './services/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FormFillerComponent } from './form-filler/form-filler.component';
 
 const dashboardChildRoutes: Routes = [
   { path: '', component: DashboardPageComponent },
+  {
+    path: 'fill-form',
+    component: FormFillerComponent,
+    canActivate: [AdminOnlyGuard],
+  },
 ];
 
 export const routes: Routes = [
---
+++ src/app/form-filler/form-filler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-filler',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-filler.component.html',
+  styleUrl: './form-filler.component.sass',
+})
+export class FormFillerComponent {}
